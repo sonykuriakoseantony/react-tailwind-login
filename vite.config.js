630aaa26cdbe4b96a99a94dd634c0a172bece492
sonykuriakoseantony <sonykuriakoseantony@gmail.com>
2025-10-16 00:26:58 +0530

    Deployment scripts added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-tailwind-login/',
   plugins: [
     react(),
     tailwindcss(),
